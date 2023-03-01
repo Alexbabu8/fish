5cf8b0e3be5dd3ab38cea54c6c94ec3c501070d1
Alexbabu8 <112873625+Alexbabu8@users.noreply.github.com>
2023-03-01 11:35:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ input_Length2 = st.slider('Diagonal length(cm)', 0.0, max(data["Length2"]), 1.0)
 input_Length3 = st.slider('Cross length(cm)', 0.0, max(data["Length3"]), 1.0)
 input_Height = st.slider('Height(cm)', 0.0, max(data["Height"]), 1.0)
 input_Width = st.slider('Diagonal width(cm)', 0.0, max(data["Width"]), 1.0)
-input_Width = st.slider('Diagonal width(cm)', 0.0, max(data["Width2"]), 1.0)
+input_Width2 = st.slider('Diagonal width(cm)', 0.0, max(data["Width2"]), 1.0)
 
 
 if st.button('Make Prediction'):
